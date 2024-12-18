19a17f637a0b529a59622f42f2d85e882d2fa139
SamuelAguiar47 <samuelsaguiar47@gmail.com>
2024-12-18 14:13:35 -0300

    Adição de um array com cadastros estáticos de usuários e respectivas senhas em valida_login.php.

--- valida_login.php
+++ valida_login.php
@@ -1,9 +1,8 @@
 <?php 
-    print_r($_POST);
-    echo "<br>";
-
-    echo $_POST['email'];
-    echo "<br>";
-    echo $_POST['senha'];
+    
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+    );
 
 ?>
