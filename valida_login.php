ba19b11296fb63c9008446f489aab083ccedd05a
SamuelAguiar47 <samuelsaguiar47@gmail.com>
2024-12-18 14:30:37 -0300

    Adição da codificação para autenticação básica dos dados de login em valida_login.php, com apenas uma mensagem exibindo se a autenticação foi bem sucedida ou se fracassou..

--- valida_login.php
+++ valida_login.php
@@ -1,8 +1,20 @@
 <?php 
-    
+    $usuario_autenticado = false;
+
     $usuarios_app = array(
         array('email' => 'adm@teste.com.br', 'senha' => '123456'),
         array('email' => 'user@teste.com.br', 'senha' => 'abcd')
     );
 
+    foreach($usuarios_app as $user) {
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autenticado!';
+    } else {
+        echo 'Erro na autenticação! ';
+    }
 ?>
